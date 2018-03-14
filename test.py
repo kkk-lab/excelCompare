5f222649153bac166534fe6071a9ca0c2ec84e54
bijiekai <bijiekai@gmail.com>
2018-03-14 19:08:03 +0800

    1

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import tkinter.filedialog
 from tkinter import messagebox
-import xlrd
-import xlwt
 import tkinter
+import xlcmp
+import xlwt
 
 
 class SelectFile():
@@ -10,7 +10,7 @@ class SelectFile():
         self.win=tkinter.Tk(screenName='400x300')
         self.win.title('Excel数据对比工具')
         self.win.resizable(0,0)
-        self.win.geometry('720x360')
+        self.win.geometry('700x300+350+150')
         self.file_1=tkinter.StringVar()
         self.file_2=tkinter.StringVar()
         self.create_widgets()
@@ -18,17 +18,18 @@ class SelectFile():
 
     def create_widgets(self):
         label_1=tkinter.Label(self.win,text='请选择文件')
-        label_1.pack()
+        # label_1.grid(column=0,row=0)
+        label_1.grid(column=0,row=0,sticky='n',padx=10,pady=20)
         name_1=tkinter.Entry(self.win,width=60,textvariable=self.file_1)
-        name_1.pack()
-        choose_1=tkinter.Button(self.win,text='Get File',command=self.get_file_1)
-        choose_1.pack()
+        name_1.grid(column=0,row=1,sticky='w',padx=60,pady=10)
+        choose_1=tkinter.Button(self.win,text='选择校验文件',command=self.get_file_1)
+        choose_1.grid(column=1,row=1,sticky='e',padx=10,pady=10)
         name_2=tkinter.Entry(self.win,width=60,textvariable=self.file_2)
-        name_2.pack()
-        choose_2=tkinter.Button(self.win,text='Next File',command=self.get_file_2)
-        choose_2.pack()
+        name_2.grid(column=0,row=5,sticky='sw',padx=60,pady=10)
+        choose_2=tkinter.Button(self.win,text='设置基准文件',command=self.get_file_2)
+        choose_2.grid(column=1,row=5,sticky='se',padx=10,pady=10)
         confirm=tkinter.Button(self.win,text='对比文件',command=self.excel_cmp)
-        confirm.pack()
+        confirm.grid(column=0,row=6,sticky='s',padx=10,pady=20)
 
     def get_file_1(self):
         self.file_1.set(tkinter.filedialog.askopenfilename(title='选择一个文件',filetypes=[('Excel文件','.xls')]))
@@ -36,7 +37,7 @@ class SelectFile():
 
     def get_file_2(self):
         self.file_2.set(tkinter.filedialog.askopenfilename(title='选择一个文件',filetypes=[('Excel文件','.xls')]))
-        response=messagebox.askretrycancel(title='Nice',message='Please confirm')
+        # response=messagebox.askretrycancel(title='Nice',message='Please confirm')
         # label_1=tkinter.Label(self.win,text=self.file_2)
         # label_1.pack()
         # print(self.file_2.get())
@@ -45,8 +46,16 @@ class SelectFile():
         if self.file_1.get()==self.file_2.get():
             responses=messagebox.askretrycancel(title='Wrong',message='请不要选择同一个文件！')
         # print('confirm')
-        pass
+        temp=xlcmp.XlCmp(self.file_1.get(),self.file_2.get())#这里要捕获异常，捕获文件占用的异常，并且弹出警告；其它的异常需要打印出来
+        if temp.same:
+            pass#表示数据一致
+        else:
+            print(temp.diff)
+            print('different!')
 
+            pass#弹出窗口下载文件
+
+        pass
 
 
 
---
+++ xlcmp.py
@@ -0,0 +1,42 @@
+import xlrd
+import xlwt
+
+class XlCmp():
+    def __init__(self,file1,file2):
+        self.file_1=file1#校验文件
+        self.file_2=file2#基准文件
+        self.same=True
+        self.diff={}
+        self.open_xls()
+
+    def open_xls(self):
+        f1=xlrd.open_workbook(self.file_1)
+        f2=xlrd.open_workbook(self.file_2)
+        f3=xlwt.Workbook()
+
+        nsheets_1=f1.nsheets
+        nsheets_2=f2.nsheets
+        # if nsheets_1!=nsheets_2:raise EOFError#后续要在前面捕获异常，并表示两个EXCEL含sheet数量不一致
+        for i in range(nsheets_2):
+            sheet_1=f1.sheet_by_index(i)
+            sheet_2=f2.sheet_by_index(i)
+            for j in range(sheet_2.nrows):
+                row_1=sheet_1.row_values(j)
+                rows_2=sheet_2.row_values(j)
+                if row_1==rows_2:continue
+                for k in range(len(rows_2)):
+                    if rows_2[k]!=row_1[k]:
+                        l=rows_2[k]
+                        self.save_location(sheet_2.name,j,k,l)
+
+
+    def save_location(self,i,j,k,l):
+        '''
+        i代表sheet，j代表行数，k代表列数
+        '''
+        self.same=False
+        temp=self.diff.get(i,[])
+        temp.append((j,k,l))
+        self.diff[i]=temp
+        print(1)
+
