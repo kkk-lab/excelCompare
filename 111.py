d407d3d3b9fd02655622c6713d73ddafdbd2bd23
bijiekai <bijiekai@gmail.com>
2018-03-14 19:09:30 +0800

    1

--- 111.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-#定义Button的回调函数
-def helloButton():
-    print('hello button')
-root = Tk()
-#通过command属性来指定Button的回调函数
-Button(root,text = 'Hello Button',command = helloButton).pack()
-root.mainloop()
